dc7278aa01fd6efcad12fa69de81b81a0d93ac67
DagoLinux <linux@dago.com>
2014-05-30 14:47:14 -0500

    Desarrollo de clase persona

---
+++ com/clases/Persona.java
@@ -0,0 +1,11 @@
+
+public class Persona{
+    private String nombre;
+    private String apellido;
+
+    public Persona(String n, String a){
+        nombre = n;
+        apellido = a;
+    }
+}
+    
